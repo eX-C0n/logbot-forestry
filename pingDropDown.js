dc45bd36dc119de7a48e9c6302a3f68a1560f808
eX-C0n <47859236+eX-C0n@users.noreply.github.com>
2025-08-12 16:41:18 -0500

    Create pingDropDown.js

---
+++ pingDropDown.js
@@ -0,0 +1,12 @@
+/*
+ *  Add Role Data from !clrbm output here - verbatim
+ */
+
+module.exports.locationDropDown = {
+  seersvillage: "1403407212646502463",
+  nmage: "1403407214106120293",
+  glade: "1403407215959867514",
+  myth: "1403407217004380180",
+  draynor: "1403407218161881138",
+  nemus: "1404937699210563779",
+}
