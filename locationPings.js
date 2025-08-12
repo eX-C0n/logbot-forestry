409b68a334fbf17ac0bb9f79ed362db987116b08
eX-C0n <47859236+eX-C0n@users.noreply.github.com>
2025-08-12 15:46:33 -0500

    Create locationPings.js

---
+++ locationPings.js
@@ -0,0 +1,11 @@
+/*
+ *  Add Role Data from !clrbm output here - verbatim
+ */
+
+module.exports.locationPings = {
+  SEERSVILLAGE: "1403407212646502463",
+  NMAGE: "1403407214106120293",
+  GLADE: "1403407215959867514",
+  MYTH: "1403407217004380180",
+  DRAYNOR: "1403407218161881138",
+}
