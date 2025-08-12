dfc3afaff15770018871142a34795f77e01a6c43
eX-C0n <47859236+eX-C0n@users.noreply.github.com>
2025-08-12 16:50:37 -0500

    Update pingDropDown.js

--- pingDropDown.js
+++ pingDropDown.js
@@ -2,11 +2,11 @@
  *  Add Role Data from !clrbm output here - verbatim
  */
 
-module.exports.locationDropDown = {
+module.exports.locationDropDown = [
   { label: 'Notify me for Seers Village', value: 'seersvillage' },
   { label: 'Notify me for Nmage', value: 'nmage' },
   { label: 'Notify me for Glade', value: 'glade' },
   { label: 'Notify me for Myth', value: 'myth' },
   { label: 'Notify me for Draynor', value: 'draynor' },
   { label: 'Notify me for Nemus', value: 'nemus' },
-}
+]
