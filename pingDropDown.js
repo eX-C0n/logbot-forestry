7d4007c06c35cf5d0e7eb4249bfe96314161762b
eX-C0n <47859236+eX-C0n@users.noreply.github.com>
2025-08-12 16:42:12 -0500

    Update pingDropDown.js

--- pingDropDown.js
+++ pingDropDown.js
@@ -3,10 +3,10 @@
  */
 
 module.exports.locationDropDown = {
-  seersvillage: "1403407212646502463",
-  nmage: "1403407214106120293",
-  glade: "1403407215959867514",
-  myth: "1403407217004380180",
-  draynor: "1403407218161881138",
-  nemus: "1404937699210563779",
+  { label: 'Notify me for Seers Village', value: 'seersvillage' },
+  { label: 'Notify me for Nmage', value: 'nmage' },
+  { label: 'Notify me for Glade', value: 'glade' },
+  { label: 'Notify me for Myth', value: 'myth' },
+  { label: 'Notify me for Draynor', value: 'draynor' },
+  { label: 'Notify me for Nemus', value: 'nemus' },
 }
