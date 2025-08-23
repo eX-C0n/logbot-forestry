44af131282a464cc14515652eaa7683e357f8562
Bass-Music <150371178+Bass-Music@users.noreply.github.com>
2025-08-23 09:26:17 -0500

    swapped nsorc for nmage

--- pingDropDown.js
+++ pingDropDown.js
@@ -1,6 +1,6 @@
 module.exports.locationDropDown = [
   { label: 'Notify me for Seers Village', value: 'seersvillage' },
-  { label: 'Notify me for Nmage', value: 'nmage' },
+  { label: 'Notify me for Nsorc', value: 'nsorc' },
   { label: 'Notify me for Glade', value: 'glade' },
   { label: 'Notify me for Myth', value: 'myth' },
   { label: 'Notify me for Draynor', value: 'draynor' },
